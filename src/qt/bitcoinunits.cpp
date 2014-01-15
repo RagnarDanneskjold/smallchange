10315562377db7aeec67c60e34aa7e9590e13b13
Sylvain Blot <sylvainblot@me.com>
2014-01-15 15:29:05 +0100

    initial rename

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("VIR");
+    case mBTC: return QString("mVIR");
+    case uBTC: return QString::fromUtf8("μVIR");
     default: return QString("???");
     }
 }
@@ -45,9 +45,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Litecoins");
-    case mBTC: return QString("Milli-Litecoins (1 / 1,000)");
-    case uBTC: return QString("Micro-Litecoins (1 / 1,000,000)");
+    case BTC: return QString("Virtualcoins");
+    case mBTC: return QString("Milli-Virtualcoins (1 / 1,000)");
+    case uBTC: return QString("Micro-Virtualcoins (1 / 1,000,000)");
     default: return QString("???");
     }
 }
