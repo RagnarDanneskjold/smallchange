681893de6bfd7dedf8e5eca91f4c8dce7836ec1a
Sylvain Blot <sylvainblot@me.com>
2014-01-15 10:10:19 -0500

    true !!!

--- src/main.cpp
+++ src/main.cpp
@@ -2787,7 +2787,7 @@ bool InitBlockIndex() {
 
 
         // If genesis block hash does not match, then generate new genesis hash.
-        if (false && block.GetHash() != hashGenesisBlock)
+        if (true && block.GetHash() != hashGenesisBlock)
         {
             printf("Searching for genesis block...\n");
             // This will figure out a valid hash and Nonce if you're
